8abb7496373dcc7c7a69a0a579c1daad0cbc0a6f
anders hviid <anders.hviid@hotmail.com>
2019-12-01 21:19:19 +0100

    knife stuff

---
+++ Assets/KnifeHot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeHot : MonoBehaviour
+{
+     
+    public bool Hot = false;
+    public bool done = false;
+    public Material fire;
+    private MeshRenderer knife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        knife = GetComponent<MeshRenderer>();
+    }
+    void Update()
+    {
+        if (Hot && !done)
+        {
+            knife.material = fire;
+            done = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Hot && collision.collider.gameObject.tag == "destroy")
+        {
+            Debug.Log("colliding");
+            Destroy(collision.collider.gameObject);
+
+        }
+
+    }
+}
